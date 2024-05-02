29e0ffe6a71018ea773187f8bd51fcdbb9e2135a
Ashish Manoj J <122615401+Ash-Manoj@users.noreply.github.com>
2024-05-02 16:02:54 +0200

    Changes to caravan.py

--- Hy2DL/datasetzoo/caravan.py
+++ Hy2DL/datasetzoo/caravan.py
@@ -1,15 +1,52 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from numba import njit, prange
-from typing import List, Tuple
+from typing import List, Optional
+from basedataset import BaseDataset
+
+
 
 # Class that contain the methods used to read the information from Caravan dataset
-class CARAVAN():
-    """Class to process the Caravans data set by [#]_ and [#]_. 
-    The Code will only work with Caravan csv files. Support is not provided for netcdf files
+class CARAVAN(BaseDataset):
+    """Class to process the Caravans data set by [#]_ . 
+    
+    The class inherits from BaseDataset to execute the operations on how to load and process the data. However here we
+    code the _read_attributes and _read_data methods, that specify how we should read the information from Caravans.
+    The code would also run with user created datasets which conform to the Caravan style convention.
+
     This class and its methods were taken from Neural Hydrology [#]_ and adapted for our specific case. 
         
+    Parameters
+    ----------
+    dynamic_input : List[str]
+        name of variables used as dynamic series input in the lstm
+    target: List[str]
+        target variable(s)
+    sequence_length: int
+        sequence length used for the model
+    time_period: List[str]
+        initial and final date (e.g. ['1987-10-01','1999-09-30']) of the time period of interest 
+    path_data: str
+        path to the folder were the data is stored
+    path_entities: str
+        path to a txt file that contain the id of the entities (e.g. catchment`s ids) that will be analyzed
+    entity: str
+        id of the entities (e.g. catchment`s id) that will be analyzed. Alternative option to specifying a
+        path_entities.
+    path_addional features: Optional[str] = None
+        Optional parameter. Allows the option to add any arbitrary data that is not included in the standard data sets.
+        Path to a pickle file (or list of paths for multiple files), containing a dictionary with each key corresponding 
+        to one basin id and the value is a date-time indexed pandas DataFrame.      
+    predict_last_n: Optional[int] = 1
+        number of timesteps (e.g. days) used to calculate the loss
+    static_input : Optional[List[str]] = []
+        name of static inputs used as input in the lstm (e.g. catchment attributes)
+    conceptual_input: Optional[List[str]] = []
+        Optional parameter. We need this when we use hybrid models. Name of variables used as dynamic series input in 
+        the conceptual model
+    check_Nan: : Optional[bool] = True
+        Boolean that indicate if one should check of NaN values while processing the data
+    
     References
     ----------
     .. [#] Kratzert, F., Nearing, G., Addor, N., Erickson, T., Gauch, M., Gilon, O., 
@@ -19,8 +56,38 @@ class CARAVAN():
         research in hydrology. Journal of Open Source Software, 7, 4050, doi: 10.21105/joss.04050, 2022 
     """
 
-    @staticmethod
-    def read_attributes(path_data: str) -> pd.DataFrame:
+
+    def __init__(self, 
+                 dynamic_input: List[str],
+                 target: List[str], 
+                 sequence_length: int,
+                 time_period: List[str],
+                 path_data: str,
+                 path_entities: str = '',
+                 entity: str = '',
+                 path_additional_features: Optional[str] = '',
+                 predict_last_n: Optional[int] = 1,
+                 static_input: Optional[List[str]] = [],
+                 conceptual_input: Optional[List[str]] = [],
+                 check_NaN:bool = True
+                 ):
+        
+        # Run the __init__ method of BaseDataset class, where the data is processed
+        super(CARAVAN, self).__init__(dynamic_input = dynamic_input,
+                                        target = target, 
+                                        sequence_length = sequence_length,
+                                        time_period = time_period,
+                                        path_data = path_data,
+                                        path_entities = path_entities,
+                                        entity = entity,
+                                        path_additional_features = path_additional_features,
+                                        predict_last_n = predict_last_n,
+                                        static_input = static_input,
+                                        conceptual_input = conceptual_input,
+                                        check_NaN=check_NaN)
+
+
+    def _read_attributes(self) -> pd.DataFrame:
         """Read the catchments` attributes from Caravan
 
         Parameters
@@ -33,7 +100,7 @@ class CARAVAN():
         df: pd.DataFrame
             Dataframe with the catchments` attributes
         """
-        data_dir = Path(path_data)
+        data_dir = Path(self.path_data)
         # Take care of the subset directories in Caravans
         subdataset_dirs = [d for d in (data_dir / "attributes").glob('*') if d.is_dir()]
 
@@ -48,12 +115,19 @@ class CARAVAN():
             dfs.append(dfr)
 
         # Merge all DataFrames along the basin index.
-        df = pd.concat(dfs, axis=0)    
+        df_attributes = pd.concat(dfs, axis=0)   
 
-        return df
+         # Encode categorical attributes in case there are any
+        for column in df_attributes.columns:
+            if df_attributes[column].dtype not in ['float64', 'int64']:
+                df_attributes[column], _ = pd.factorize(df_attributes[column], sort=True) 
+
+         # Filter attributes and basins of interest
+        df_attributes = df_attributes.loc[self.entities_ids, self.static_input]
+
+        return df_attributes
     
-    @staticmethod
-    def read_data(path_data: str, catch_id: str)-> pd.DataFrame:
+    def _read_data(self, catch_id: str)-> pd.DataFrame:
         """Loads the timeseries data of one basin from the Caravan dataset.
     
         Parameters
@@ -69,7 +143,7 @@ class CARAVAN():
         df: pd.DataFrame
             Dataframe with the catchments` timeseries
         """
-        data_dir = Path(path_data)
+        data_dir = Path(self.path_data)
         basin = catch_id
 
         # Get the subdataset name from the basin string.
